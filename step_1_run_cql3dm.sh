0aa6a7a5ed19c0501848d73def5c41ab6ef2025b
Juan Francisco Caneses-Marin <caneses@compx.com>
2024-11-05 13:03:05 +1000

    We are passing both the fidasim and cql3d run directories as inputs to functions to run preprocessor, FIDASIM and produce birth points in text file. Need to update remote so taht we can test workflow in NERSC

---
+++ step_1_run_cql3dm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Setup CONDA python environment:
+# ===========================================================
+# This is required to run the FIDASIM-CQL3DM preprocessor
+
+# Initialize Conda
+source ~/miniconda3/etc/profile.d/conda.sh
+
+# Activate the Conda environment
+conda activate FIDASIM_env
+
+# Define run directories:
+# ===========================================================
+export RUN_ID="WHAM_no_f4d"
+export FIDASIM_RUN_DIR="$PWD/fidasim_files/$RUN_ID"
+export CQL3D_RUN_DIR="$PWD/cql3d_files/$RUN_ID"
+export START_DIR=$PWD
+
+# Setup environmental variables:
+# ===========================================================
+# Define the directory where the Forge binaries are located
+FORGE_DIR=/home/jfcm/linaro/forge/24.0.2/bin
+
+# Export FORGE_DIR to the PATH
+export PATH=$FORGE_DIR:$PATH
+
+# Number of processors:
+NUM_PROCS=18 # Number of MPI processes for CQL3D
+export NUM_THREADS=14 # Number of theads for FIDASIM
+
+# Initialize run and time it:
+cd $CQL3D_RUN_DIR
+#time mpirun -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64 > log.txt
+time mpirun -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64
+cd $START_DIR

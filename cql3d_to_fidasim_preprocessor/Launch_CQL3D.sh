7c9f51c3a990b8df1eaa1433a6be273686452b53
Juan Francisco Caneses-Marin <caneses@compx.com>
2025-02-13 21:26:03 +1000

    Added capability autodetect if we are running on NERSC to choose SRUN from MPIRUN on perlmutter

--- cql3d_to_fidasim_preprocessor/Launch_CQL3D.sh
+++ cql3d_to_fidasim_preprocessor/Launch_CQL3D.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Prevent direct execution
+# Prevent direct execution:
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     echo "This shell script (Launch_CQL3D.sh) cannot be run directly. It must be sourced or executed by another shell script which provides the required env vars."
     echo "Environmental variables required are the following:"
@@ -11,6 +11,16 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     echo ""
     echo "To run this script, Use step_1_run_cql3dm.sh or a similar script"
     exit 1
+fi
+
+# Check if running on NERSC perlmutter:
+# ===========================================================
+if [[ "$NERSC_HOST" == "perlmutter" ]]; then
+    echo "Running on NERSC Perlmutter"
+    MPI_CMD="srun"
+else
+    echo "Running from" $HOSTNAME
+    MPI_CMD="mpirun"
 fi
 
 # Export environmental variables:
@@ -26,42 +36,22 @@ export PREPROCESSOR_PLOT
 
 # Run CQL3D:
 # ===========================================================
-# Initialize run and time it:
-# cd $CQL3D_RUN_DIR
-# if [[ "$CLI_TO_TXT" == "1" ]]; then
-#     echo "CLI_TO_TXT is set to 1. Recording CLI output to text file log.txt ..."
-#     time mpirun -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64 > log.txt 2>&1 &
-# elif [[ "$CLI_TO_TXT" == "0" ]]; then
-#       echo "CLI_TO_TXT is set to 0. NOT Recording CLI output ..."
-#     time mpirun -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64
-# elif [[ -z "$CLI_TO_TXT" ]]; then
-#     echo "CLI_TO_TXT is empty. Do not record CLI output to text file ..."
-#     time mpirun -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64
-# else
-#     echo "CLI_TO_TXT has an unexpected value: $CLI_TO_TXT"
-#     echo "CQL3D run has been terminated ..."
-#     # Handle any other unexpected values if necessary
-# fi
-# cd $START_DIR
-
-# Run CQL3D:
-# ===========================================================
 if [[ "$DEBUG_CQL3D" == "1" ]]; then
     echo "DEBUG_CQL3D is set to 1. Running CQL3D in debug mode with ddt..."
     cd $CQL3D_RUN_DIR
-    time ddt --connect mpirun -n $NUM_PROCS $CQL3DM_DIR/$EXECUTABLE
+    time ddt --connect $MPI_CMD -n $NUM_PROCS $CQL3DM_DIR/$EXECUTABLE
     cd $START_DIR
 else
     cd $CQL3D_RUN_DIR
     if [[ "$CLI_TO_TXT" == "1" ]]; then
         echo "CLI_TO_TXT is set to 1. Recording CLI output to text file log.txt ..."
-        time mpirun -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64 > log.txt 2>&1 &
+        time $MPI_CMD -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64 > log.txt 2>&1 &
     elif [[ "$CLI_TO_TXT" == "0" ]]; then
         echo "CLI_TO_TXT is set to 0. NOT Recording CLI output ..."
-        time mpirun -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64
+        time $MPI_CMD -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64
     elif [[ -z "$CLI_TO_TXT" ]]; then
         echo "CLI_TO_TXT is empty. Do not record CLI output to text file ..."
-        time mpirun -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64
+        time $MPI_CMD -n $NUM_PROCS $CQL3DM_DIR/xcql3dm_mpi.gfortran64
     else
         echo "CLI_TO_TXT has an unexpected value: $CLI_TO_TXT"
         echo "CQL3D run has been terminated ..."
